dd8fa4f61db18b276c92ec4dfac689801a85e4bc
n3rdium <n3rdium@gmail.com>
2026-01-22 19:41:26 +0530

    [dev] docstrings for the compute manager

--- voxl/core/compute.py
+++ voxl/core/compute.py
@@ -1,5 +1,8 @@
-"""Utilities for easy usage of compute shaders in the engine."""
+"""Minimal utilities providing compute shaders."""
 
+# TODO improve docstrings in this file.
+
+import numpy as np
 from typing_extensions import TypedDict
 from logging import Logger, getLogger
 from voxl.types import ComputeBindings
@@ -12,6 +15,7 @@ from wgpu.classes import (
     GPUComputePassEncoder,
     GPUComputePipeline,
     GPUDevice,
+    GPUShaderModule,
 )
 from wgpu.structs import BindGroupEntry
 
@@ -19,17 +23,20 @@ from wgpu.structs import BindGroupEntry
 class ComputeManagerConfig(TypedDict):
     """The compute manager configuration TypedDict."""
 
+    # TODO configurability
+    ...
+
 
 class ComputeManager:
-    """Minimal compute shader API
-    """
+    """Minimal wgpu compute manager."""
+
     config: ComputeManagerConfig
     adapter: GPUAdapter
     device: GPUDevice
 
     def __init__(self, config: ComputeManagerConfig) -> None:
-        """TODO write docstrings once the thing is actually made.
-        """
+        """Initialize the compute manager."""
+
         self.config = config
         self.logger: Logger = getLogger("ComputeManager")
 
@@ -41,38 +48,106 @@ class ComputeManager:
 
         self.dispatch_queue = []
 
-    def run_compute_pass(self):
-        # do n items from the queue
-        pass
+    def compute_pass(self) -> None:
+        """Run a single compute pass.
 
-# TODO compute shader code as assets.
+        Handles command encoders, does the compute pass and dispatches items
+        from the dispatch queue.
+        """
+
+        encoder = self.device.create_command_encoder()
+        pass_encoder = encoder.begin_compute_pass()
+
+        for item in self.dispatch_queue:
+            pipeline, kwargs = item
+            pipeline.dispatch(pass_encoder, **kwargs)
+
+        pass_encoder.end()
+        self.device.queue.submit([encoder.finish()])
+
+    def enqueue(self, pipeline, **kwargs) -> None:
+        """Enqueue a compute task.
+
+        Stores the kwargs (apart from pipeline) in the dispatch queue. The
+        kwargs other than pipeline are passed straight to pipeline.dispatch().
+        For more info, see
+        :py:meth:`voxl.core.compute.ComputePipeline.dispatch`.
+
+        Args:
+            pipeline (ComputePipeline): The compute pipeline to use.
+            bindings: the buffer bindings to use.
+            n_workgroups: number of workgroups to dispatch.
+        """
+
+        self.dispatch_queue.append((pipeline, kwargs))
+
+    def buffer_from_np(
+        self,
+        data: np.ndarray,
+        usage: int = wgpu.BufferUsage.STORAGE
+        | wgpu.BufferUsage.COPY_SRC
+        | wgpu.BufferUsage.COPY_DST,
+    ) -> GPUBuffer:
+        """Create a GPUBuffer from a given numpy array."""
+
+        return self.device.create_buffer_with_data(
+            data=data.tobytes(), usage=usage
+        )
+
+    def readback(
+        self, buffer: GPUBuffer, nbytes: int, dtype: str
+    ) -> np.ndarray:
+        """Read back the results of the computation into a numpy array."""
+
+        readback_buffer = self.device.create_buffer(
+            size=nbytes,
+            usage=wgpu.BufferUsage.COPY_DST | wgpu.BufferUsage.MAP_READ,
+        )
+
+        encoder = self.device.create_command_encoder()
+        encoder.copy_buffer_to_buffer(buffer, 0, readback_buffer, 0, nbytes)
+        self.device.queue.submit([encoder.finish()])
+
+        result = self.device.queue.read_buffer(buffer)
+        return np.array(result.cast(dtype))
+
+
 class ComputePipeline:
+    """Handles wgpu compute pipeline creation for a compute shader."""
+
     shader: str
-    bindings: ComputeBindings
     manager: ComputeManager
     pipeline: GPUComputePipeline
+    module: GPUShaderModule
 
     def __init__(
         self,
         shader: str,
         entry_point: str,
-        bindings: ComputeBindings,
         manager: ComputeManager,
     ) -> None:
+        """Compiles the shader module and creates a wgpu compute pipeline."""
+
         self.shader = shader
-        self.bindings = bindings
         self.manager = manager
 
         device = manager.device
+        self.module = device.create_shader_module(code=shader)
         self.pipeline = device.create_compute_pipeline(
             layout="auto",
-            compute={"module": self.shader, "entry_point": entry_point},
+            compute={"module": self.module, "entry_point": entry_point},
         )
 
     def _build_groups(
-        self,
-        bindings: ComputeBindings
+        self, bindings: ComputeBindings
     ) -> dict[int, GPUBindGroup]:
+        """Convert the simplified binding format into a wgpu-compatible struct.
+
+        Args:
+            bindings (ComputeBindings): dictionary of bindings. See
+                :py:data:`voxl.types.ComputeBindings` for more.
+        """
+
         groups: dict[int, GPUBindGroup] = {}
 
         for group_id in bindings:
@@ -87,12 +162,16 @@ class ComputePipeline:
         return groups
 
     def _build_group_entries(
-        self, binding: dict[int, GPUBuffer]
+        self, group: dict[int, GPUBuffer]
     ) -> list[BindGroupEntry]:
+        """Build the buffer binding entries for a given bind group."""
+
         entries: list[BindGroupEntry] = []
 
-        for entry in entries:
-            entries.append({"binding": entry, "resource": binding[entry]})
+        for buffer_idx in group:
+            entries.append(
+                {"binding": buffer_idx, "resource": group[buffer_idx]}
+            )
 
         return entries
 
@@ -100,19 +179,30 @@ class ComputePipeline:
         self,
         pass_encoder: GPUComputePassEncoder,
         bindings: ComputeBindings,
-        n_workgroups: tuple[int, int, int] = (1, 1, 1)
+        n_workgroups: tuple[int, int, int] = (1, 1, 1),
     ) -> None:
+        """Dispatch the pipeline with the given data and n_workgroups
+
+        Sets the pipeline to self, handles bind groups, and dispatches the
+        pipeline to the pass encoder provided by
+        :py:meth:`voxl.core.compute.ComputeManager.compute_pass`
+
+        Args:
+            pass_encoder: a compute pass encoder object.
+            bindings: the buffer bindings to use. See
+                :py:data:`voxl.types.ComputeBindings` for more.
+            n_workgroups: number of workgroups to dispatch.
+        """
         pass_encoder.set_pipeline(self.pipeline)
 
-        bind_groups: dict[int, GPUBindGroup]  = self._build_groups(bindings)
+        bind_groups: dict[int, GPUBindGroup] = self._build_groups(bindings)
         for group_idx in bind_groups:
             pass_encoder.set_bind_group(
                 group_idx,
                 bind_groups[group_idx],
                 dynamic_offsets_data=[],
                 dynamic_offsets_data_start=0,
-                dynamic_offsets_data_length=999999 # todo what?!
+                dynamic_offsets_data_length=999999,  # todo what?!
             )
 
         pass_encoder.dispatch_workgroups(*n_workgroups)
-
