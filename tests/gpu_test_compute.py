0e1c43716232606d9c74ccb8a7e2ede534962ff4
n3rdium <n3rdium@gmail.com>
2026-01-22 22:24:24 +0530

    [dev] format files with ruff

--- tests/gpu_test_compute.py
+++ tests/gpu_test_compute.py
@@ -6,9 +6,7 @@ from voxl.core.compute import ComputeManager, ComputePipeline
 
 class TestCompute(unittest.TestCase):
     def test_double(self):
-        manager = ComputeManager({
-            "power_preference": "high-performance"
-        })
+        manager = ComputeManager({"power_preference": "high-performance"})
 
         shader = """
         @group(0) @binding(0)
