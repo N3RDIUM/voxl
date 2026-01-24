3b5387e5e3157d9a3c782823993f251dfa23dd66
n3rdium <n3rdium@gmail.com>
2026-01-24 10:00:27 +0530

    [dev] include OpenGLShader in  __init__

--- voxl/core/renderer/opengl/__init__.py
+++ voxl/core/renderer/opengl/__init__.py
@@ -3,10 +3,12 @@
 from .opengl import OpenGLRenderer, OpenGLConfig
 from .buffer import Buffer
 from .quad_mesh import QuadMesh
+from .shader import OpenGLShader
 
 __all__ = [
     "OpenGLRenderer",
     "OpenGLConfig",
     "Buffer",
     "QuadMesh",
+    "OpenGLShader"
 ]
