85562a05ac6f2bd515def4b7ce27d8f6fcb36a15
n3rdium <n3rdium@gmail.com>
2026-02-13 14:26:28 +0530

    fix: ecs optimizations

--- src/engine/ecs.py
+++ src/engine/ecs.py
@@ -1,9 +1,10 @@
-from collections.abc import Collection, Generator
+from collections.abc import Collection
 from dataclasses import dataclass
-from typing import Generic, TypeVar, cast
+from typing import TypeVar, cast
 
-type EntityID = int
+type Entity = int
 type RowIndex = int
+type Archetype = int
 
 
 @dataclass
@@ -13,35 +14,17 @@ class Component: ...
 C = TypeVar("C", bound=Component)
 
 
-@dataclass(frozen=True)
-class Entity:
-    id: EntityID
-
-
-type Archetype = int
-
-
-class ComponentLookup(Generic[C]):
-    _data: dict[type[Component], list[Component]]
-    _row: int
-
-    def __init__(self, data: dict[type[Component], list[Component]], row: int):
-        self._data = data
-        self._row = row
-
-    def __getitem__(self, key: type[C]) -> C:
-        return cast(C, self._data[key][self._row])
-
-
-class ECS:  # TODO document
-    _next_entity_id: EntityID
+class ECS:
+    _next_entity_id: Entity
     _next_archetype: Archetype
 
     entities: dict[Entity, tuple[Archetype, RowIndex]]
-    entity_rows: dict[Archetype, dict[RowIndex, Entity]]
+    entity_rows: dict[Archetype, list[Entity]]
     components: dict[Archetype, dict[type[Component], list[Component]]]
+
     archetypes: dict[frozenset[type[Component]], Archetype]
     archetypes_reverse: dict[Archetype, frozenset[type[Component]]]
+    archetypes_by_component: dict[type[Component], set[Archetype]]
 
     def __init__(self):
         self._next_entity_id = 0
@@ -50,18 +33,20 @@ class ECS:  # TODO document
         self.entities = {}
         self.entity_rows = {}
         self.components = {}
+
         self.archetypes = {}
         self.archetypes_reverse = {}
+        self.archetypes_by_component = {}
 
     def spawn(self) -> Entity:
         empty_archetype = self.determine_archetype(frozenset())
         row: RowIndex = len(self.entity_rows[empty_archetype])
 
-        entity = Entity(id=self._next_entity_id)
+        entity = self._next_entity_id
         self._next_entity_id += 1
 
         self.entities[entity] = (empty_archetype, row)
-        self.entity_rows[empty_archetype][row] = entity
+        self.entity_rows[empty_archetype].append(entity)
 
         return entity
 
@@ -77,13 +62,17 @@ class ECS:  # TODO document
         except KeyError:
             pass
 
-        self.archetypes[component_types] = self._next_archetype
-        self.archetypes_reverse[self._next_archetype] = component_types
         archetype = self._next_archetype
         self._next_archetype += 1
 
+        self.archetypes[component_types] = archetype
+        self.archetypes_reverse[archetype] = component_types
+
+        for t in component_types:
+            self.archetypes_by_component.setdefault(t, set()).add(archetype)
+
         self.components[archetype] = {t: [] for t in component_types}
-        self.entity_rows[archetype] = {}
+        self.entity_rows[archetype] = []
 
         return archetype
 
@@ -101,20 +90,22 @@ class ECS:  # TODO document
             empty_archetype = self.determine_archetype(frozenset())
             row = len(self.entity_rows[empty_archetype])
             self.entities[entity] = (empty_archetype, row)
-            self.entity_rows[empty_archetype][row] = entity
+            self.entity_rows[empty_archetype].append(entity)
             return
 
         types = self.component_types(components)
         archetype = self.determine_archetype(types)
+
         self._remove_components(entity)
 
-        first_type: type[Component] = type(components[0])
         for component in components:
             self.components[archetype][type(component)].append(component)
 
+        first_type = type(components[0])
         row: RowIndex = len(self.components[archetype][first_type]) - 1
+
         self.entities[entity] = (archetype, row)
-        self.entity_rows[archetype][row] = entity
+        self.entity_rows[archetype].append(entity)
 
     def _remove_components(self, entity: Entity) -> None:
         archetype, row = self.entities[entity]
@@ -124,12 +115,10 @@ class ECS:  # TODO document
             last_row = len(self.entity_rows[archetype]) - 1
 
             if row == last_row:
-                del self.entity_rows[archetype][last_row]
+                _ = self.entity_rows[archetype].pop()
                 return
 
-            last_entity = self.entity_rows[archetype][last_row]
-            del self.entity_rows[archetype][last_row]
-
+            last_entity = self.entity_rows[archetype].pop()
             self.entity_rows[archetype][row] = last_entity
             self.entities[last_entity] = (archetype, row)
             return
@@ -140,7 +129,7 @@ class ECS:  # TODO document
         if row == last_row:
             for component_type in self.components[archetype]:
                 _ = self.components[archetype][component_type].pop()
-            del self.entity_rows[archetype][last_row]
+            _ = self.entity_rows[archetype].pop()
             return
 
         last_components: dict[type[Component], Component] = {}
@@ -149,24 +138,46 @@ class ECS:  # TODO document
                 component_type
             ].pop()
 
-        last_entity = self.entity_rows[archetype][last_row]
-        del self.entity_rows[archetype][last_row]
+        last_entity = self.entity_rows[archetype].pop()
 
         self.entity_rows[archetype][row] = last_entity
         self.entities[last_entity] = (archetype, row)
+
         for component_type, component in last_components.items():
             self.components[archetype][component_type][row] = component
 
     def query(
         self, component_types: Collection[type[C]]
-    ) -> Generator[tuple[Entity, ComponentLookup[C]], None, None]:
-        required_types = frozenset(component_types)
+    ) -> tuple[list[Entity], dict[type[C], list[C]]]:
+        required = list(component_types)
+        if not required:
+            return [], {}
 
-        for archetype_set, archetype in self.archetypes.items():
-            if not archetype_set.issuperset(required_types):
-                continue
-            for row, entity in self.entity_rows[archetype].items():
-                yield (
-                    entity,
-                    ComponentLookup(self.components[archetype], row),
-                )
+        candidate_sets = [self.archetypes_by_component.get(t) for t in required]
+
+        if not candidate_sets or any(s is None for s in candidate_sets):
+            return [], {}
+
+        first_set = candidate_sets[0]
+        assert first_set is not None
+        matching_archetypes = first_set.copy()
+        for s in candidate_sets[1:]:
+            assert s is not None
+            matching_archetypes &= s
+
+        if not matching_archetypes:
+            return [], {}
+
+        result_entities: list[Entity] = []
+        result_components: dict[type[C], list[C]] = {t: [] for t in required}
+
+        for archetype in matching_archetypes:
+            entity_list = self.entity_rows[archetype]
+            component_data = self.components[archetype]
+
+            result_entities.extend(entity_list)
+
+            for t in required:
+                result_components[t].extend(cast(list[C], component_data[t]))
+
+        return result_entities, result_components
